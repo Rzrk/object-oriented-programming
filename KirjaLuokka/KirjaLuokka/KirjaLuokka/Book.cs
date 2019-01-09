363f6563fa97a8a3eb508212511e99b36699a09c
matpyy <matias.pyyhtia09@gmail.com>
2019-01-09 10:12:51 +0200

    Added Kirjaluokka

---
+++ KirjaLuokka/KirjaLuokka/KirjaLuokka/Book.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KirjaLuokka
+{
+    class Book
+    {
+       
+        public string Title;
+        public string Author;
+        public string Id;
+        public double Price;
+
+        public Book(string Title, string Author, string Id, double Price)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Id = Id;
+            this.Price = Price;
+        }
+
+        public Book()
+        {
+        }
+
+        internal string PrintInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string CompareBook(Book newBook2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+  
+    public string PrintInfo()
+    {
+        return "Nimi: " + this.Title +
+              "\nKirjoittaja: " + this.Author +
+              "\nID" + this.Id + "Hinta: " + this.Price;
+    }
+
+
+    public string CompareBook(Book book)
+    {
+        string text = "";
+        if(this.Price > Book.Price)
+        {
+            text = "";
+        }
+        else
+        {
+            text = "";
+        }
+        return text;
+
+    }
+}
+
+
+
+
+
+
+
+
---
+++ KirjaLuokka/KirjaLuokka/KirjaLuokka/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KirjaLuokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Kirjat:");
+
+            Book myBook = new Book();
+            string text = myBook.PrintInfo();
+
+
+            Book newBook = new Book("aapinen",
+                                    "Mikael Agricola",
+                                    "0001",
+                                    22.50);
+
+            Console.WriteLine ("\n**************" + newBook.PrintInfo());
+
+            Book newBook2 = new Book("Peppi Pitkätossu", "Astrid Lindgren", "0002", 18.95);
+            Console.WriteLine(newBook2.PrintInfo());
+
+            /* tapa 1
+            newBook.CompareBook(newBook2);
+            Console.WriteLine(newBook.CompareBook(newBook2));*/
+
+            //tapa2
+
+            string compareText = newBook.CompareBook(newBook2);
+            {
+                Console.WriteLine(compareText);
+            }
+
+
+
+
+
+        }
+    }
+}
