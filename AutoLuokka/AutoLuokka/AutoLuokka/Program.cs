f14081b686f0e9e542239470163d69db237f0434
matpyy <matias.pyyhtia09@gmail.com>
2019-01-11 12:20:43 +0200

    Added autoluokka

---
+++ AutoLuokka/AutoLuokka/AutoLuokka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoLuokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
